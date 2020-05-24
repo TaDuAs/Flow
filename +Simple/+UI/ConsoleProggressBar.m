66f810839a8d6c32688c6f66273b9c7a3f7b6655
TaDuAs <tal.duanias@mail.huji.ac.il>
2020-05-24 10:13:19 +0300

    1

--- +Simple/+UI/ConsoleProggressBar.m
+++ +Simple/+UI/ConsoleProggressBar.m
@@ -22,6 +22,7 @@ classdef ConsoleProggressBar < handle
     methods
         function this = ConsoleProggressBar(actionName, endAt, alertInterval,...
                 showEachIteration, iterationChar)
+            Simple.obsoleteWarning('Simple.UI');
             this.actionName = actionName;
             this.endAt = endAt;
             if nargin > 2
@@ -33,7 +34,7 @@ classdef ConsoleProggressBar < handle
             if nargin > 4 && ~isempty(iterationChar)
                 this.iterationChar = iterationChar;
             end
-            this.stopper = util.Stopwatch();
+            this.stopper = Simple.Stopwatch();
         end
         
         function reportProggress(this, howMuch)
