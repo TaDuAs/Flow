bcd4c82c50c16d1160781d4f3242d8e3387af750
TaDuAs <47486731+TaDuAs@users.noreply.github.com>
2020-10-29 15:26:24 +0200

    Update xyarrow.m

--- FlowUI/UIControls/Utilities/+sui/xyarrow.m
+++ FlowUI/UIControls/Utilities/+sui/xyarrow.m
@@ -13,28 +13,32 @@ function ah = xyarrow(varargin)
 % Output:
 %   ah - the arrow annotation handle
 %
-% ah = xyarrow(axes, x, y)
-%   adds an arrow annotation to the specified axes handle.
+% ah = xyarrow(x, y, annotationType)
+%   also takes in an argument which determines the annotation type
 % Input:
-%   ax - The axes object on which to overlay the arrow annotation
 %   x - A 2 element vector containing the x-coordinates of the arrow within
 %       the current axes object, of the form [xstart xend]
 %   y - A 2 element vector containing the y coordinates of the arrow within
 %   	the current axes object, of the form [ystart yend]
+%   annotationType - The type of annotation object to create.
+%                    ('line' | 'arrow' | 'doublearrow' | 'textarrow' | 'rectangle' | 'ellipse' | 'textbox')
 % Output:
 %   ah - the arrow annotation handle
 %
-% ah = xyarrow(figure, x, y)
+% ah = xyarrow(axes, ___)
+%   adds an arrow annotation to the specified axes handle.
+% Input:
+%   ax - The axes object on which to overlay the arrow annotation
+% Output:
+%   ah - the arrow annotation handle
+%
+% ah = xyarrow(figure, ___)
 %   adds an arrow annotation to the active axes handle within the specified
 %   figure. If no axes object exists, one will be created.
 % Input:
 %   figure - The figure handle to which add the arrow annotation. The
 %            annotation position will be calculated according to the x-y
 %            axes of the active axes object within this figure.
-%   x - A 2 element vector containing the x-coordinates of the arrow within
-%       the current axes object, of the form [xstart xend]
-%   y - A 2 element vector containing the y coordinates of the arrow within
-%   	the current axes object, of the form [ystart yend]
 % Output:
 %   ah - the arrow annotation handle
 %
